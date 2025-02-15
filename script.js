fe70b98e68c96a2ddca7e3060c9b83b607134b03
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2025-02-15 12:08:04 +0700

    Styled books list table

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-let heading = document.querySelector("h1");
+// Array for storing books
+const myLibrary = [];
 
-heading.style.backgroundColor = "black";
-heading.style.color = "white";
+// Constructor for Books
+function Book(title, author, numPages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, numPages, isRead) {
+    const book = new Book(title, author, numPages, isRead);
+
+    myLibrary.push(book);
+}
+
+function initializeMyLibrary() {
+    addBookToLibrary("Elon Musk", "Walter Isaacson", 688, true);
+    addBookToLibrary("Filosofi Teras", "Henry Manampiring", 1120, false);
+    addBookToLibrary("Sapiens", "Yuval Noah Harari", 1520, true);
+    addBookToLibrary("Bumi Manusia", "Pramoedya Ananta Toer", 330, true);
+}
+
+function displayLibrary() {
+
+}
